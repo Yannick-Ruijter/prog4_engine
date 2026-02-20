22b6e4a43cec1485448fd0110fae28c93b842e0b
yannick ruijter <ruyter6969@gmail.com>
2026-02-20 16:49:09 +0100

    deleted Copy/move constructor/assignment operator

--- Minigin/Component.h
+++ Minigin/Component.h
@@ -6,6 +6,10 @@ namespace dae
 	class Component {
 	public:
 		virtual ~Component() = default;
+		Component(Component const& other) = delete;
+		Component(Component&& other) = delete;
+		Component& operator=(Component const& other) = delete;
+		Component& operator=(Component&& other) = delete;
 		virtual void Update(float deltaTime);
 		virtual void Render() const;
 	protected:
