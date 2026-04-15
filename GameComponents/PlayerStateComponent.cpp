98a7b37e64bab2cc2fab250a182731bb6db08367
yannick ruijter <ruyter6969@gmail.com>
2026-04-15 21:34:26 +0200

    fixed emscriptem error

--- GameComponents/PlayerStateComponent.cpp
+++ GameComponents/PlayerStateComponent.cpp
@@ -10,6 +10,7 @@ dae::PlayerStateComponent::PlayerStateComponent(GameObject& owner)
 
 void dae::PlayerStateComponent::Notify(unsigned int eventId, GameObject*)
 {
+	//add functionality for checking how many starts and stops have been detected to see if we're still moving
 	if (eventId == "OnPlayerStartedMoving"_h)
 	{
 		m_CurrentPlayerState = PlayerState::Running;
